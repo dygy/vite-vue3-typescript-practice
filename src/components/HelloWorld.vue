f3ce1ba7ba2224ced26ce1ff9e5de4cb0adf5be8
Arkadii Shvartsman <a.shvartcman@gmail.com>
2024-01-15 20:53:57 +0100

    validation and ui kit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
+import { useCounterStore } from "../store/counter.ts";
 import Options from "./Options.vue";
 import Composition from "./Composition.vue";
-import { useCounterStore } from "../store/counter.ts";
 import { computed } from "vue";
+import { useForm } from "vee-validate";
 const store = useCounterStore();
 const isEven = computed(() => store.isEven);
 const count = computed(() => store.count);
+
+function required(value?: string) {
+    return value ? true : "This field is required";
+}
+const { defineField, handleSubmit, errors } = useForm({
+    validationSchema: {
+        field: required,
+    },
+});
+const [field, fieldProps] = defineField("field");
 </script>
 
 <template>
     <div class="card">
-        <button type="button" @click="store.increment">count is {{ count }}</button>
+        <VanButton type="primary" @click="store.increment"> count is {{ count }} </VanButton>
+        <input v-model="field" v-bind="fieldProps" />
+        <span class="error">{{ errors.field }}</span>
+        <VanButton type="danger" @click="handleSubmit"> submit </VanButton>
         <keep-alive>
             <options v-if="isEven" />
             <composition v-else />
@@ -20,6 +34,9 @@ const count = computed(() => store.count);
 
 <style scoped>
 div {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
     background: indigo;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from "pinia";
 import "./style.css";
 import HelloWorld from "./components/HelloWorld.vue";
 import App from "./App.vue";
+import { Button } from "vant";
 
 const app = createApp(App);
 
@@ -21,4 +22,5 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(Button);
 app.mount("#app");

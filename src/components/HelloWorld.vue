21ceff47b37f25739b9c1ddd0a8a9a1cb962e333
Arkadii Shvartsman <arkadii.shvartcman@tvbet.tv>
2024-01-15 21:18:34 +0100

    lazy load

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,6 +29,20 @@ const [field, fieldProps] = defineField("field");
             <options v-if="isEven" />
             <composition v-else />
         </keep-alive>
+        <div
+            v-lazy-container="{
+                selector: 'img',
+                error: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Icon_Simple_Error.png',
+                loading:
+                    'https://media0.giphy.com/media/uIJBFZoOaifHf52MER/200w.gif?cid=6c09b952rg4l0n4dituadt53hhlj3iamviq1kc2rt1mdx427&ep=v1_gifs_search&rid=200w.gif&ct=g',
+            }"
+        >
+            <img
+                data-src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-james-wheeler-417074.jpg&fm=jpg"
+            />
+            <img data-src="https://starwalk.space/gallery/images/what-is-space/1920x1080.jpg" />
+            <img data-src="https://cdn.britannica.com/79/149179-050-DC23D823/snowflake-threads-wool-coat.jpg" />
+        </div>
     </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import "./style.css";
 import HelloWorld from "./components/HelloWorld.vue";
 import App from "./App.vue";
 import { Button } from "vant";
+import VueLazyload from "vue-lazyload";
 
 const app = createApp(App);
 
@@ -23,4 +24,5 @@ const pinia = createPinia();
 app.use(pinia);
 app.use(router);
 app.use(Button);
+app.use(VueLazyload);
 app.mount("#app");
